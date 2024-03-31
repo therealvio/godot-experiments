6053dd3a5e0b5a7a1f6d6db921f66676194e4db7
therealvio <41095688+therealvio@users.noreply.github.com>
2024-04-01 09:41:34 +1100

    feat: add impulse-driven movement player

--- character-movement-types/scripts/ImpulseCharacter.gd
+++ character-movement-types/scripts/ImpulseCharacter.gd
@@ -1,21 +1,24 @@
 extends CharacterBody2D
 
-
 const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -400.0
+
+# What movement are we using?
+var label: String = "Impulse"
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var label_node: Label = $Label as Label
+
+func _ready() -> void:
+	# Set the label to the movement type.
+	label_node.text = label
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
